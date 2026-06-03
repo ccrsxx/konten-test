ad4f0a515a6187d07e3b09c7d8967bc9831fc277
Ami <ami@ccrsxx.com>
2026-06-03 15:12:34 +0700

    fix(config): add allowedDevOrigins to next configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true
+  reactCompiler: true,
+  allowedDevOrigins: ['192.168.1.2', 'dev-frontend.ccrsxx.my.id']
 };
 
 export default nextConfig;
